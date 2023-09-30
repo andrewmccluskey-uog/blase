d5c2e4e50a66d16b9bca0dd797b2ee4f26c91ad8
Andrew McCluskey <andrew@cluskii.com>
2023-09-30 17:15:03 +0100

    Add AtgnatData class

---
+++ R/atgnat_data_class.R
@@ -0,0 +1,111 @@
+#' Atgnat Data Object
+#'
+#' @slot pseudobulks data.frame. Each column is a timepoint sample and each row is a gene.
+#' @slot bins list. A list of bin names for each timepoint.
+#' @slot genes list. A list of the genes selected for discriminating timepoints.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+setClass(
+  Class = "AtgnatData",
+  slots = list(
+    pseudobulks = "data.frame",
+    bins = "numeric",
+    genes = "character"
+  )
+)
+
+#' @title Show an AtgnatData object
+#' @param object an AtgnatData object
+#' @export
+setMethod(f = "show",
+          signature = "AtgnatData",
+          definition = function(object){
+
+            output = c(
+              "Atgnat Data with:",
+              paste("\tpseudobulks count:", ncol(object@pseudobulks)),
+              paste("\ttotal genes:", nrow(object@pseudobulks)),
+              paste("\tbins:", list(object@bins)),
+              paste("\tselected genes:", object@genes)
+            )
+
+            cat(paste(output, collapse = '\n'))
+          })
+
+#' @title Conversion to AtgnatData
+#' @rdname as.AtgnatData
+#' @param x An object to take counts from
+#' @param ... additional arguments passed to object-specific methods.
+#'
+#' @return An `AtgnatData` object
+#'
+#' @export
+setGeneric(name = "as.AtgnatData",
+           signature = c("x"),
+           def = function(x, ...) standardGeneric("as.AtgnatData"))
+
+#' @rdname as.AtgnatData
+#' @param pseudotime_slot The SingleCellExperiment slot containing the pseudotime values for each cell
+#' @param n_bins The number of bins to create.
+#' @param split_by The technique used to split the bins. The default "pseudotime_range" picks the
+#' bin for a cell based on a constant range of pseudotime. "cells" picks the bin for a cell based on
+#' an even number of cells per bin.
+#'
+#' @export
+setMethod(
+  f = "as.AtgnatData",
+  signature = c(x="SingleCellExperiment"),
+  definition = function(x, pseudotime_slot="slingPseudotime_1", n_bins=20, split_by="pseudotime_range"){
+
+    if (is.na(match(split_by, c("pseudotime_range", "cells")))) {
+      stop("split_by must be 'pseudotime_range' or 'cells'")
+    }
+
+    # TODO add check for pseudotime_slot existing
+
+    pseudotime_sce = subset(x, , !is.na(x@colData[pseudotime_slot]))
+    pseudotime = pseudotime_sce@colData[[pseudotime_slot]]
+
+    c = SingleCellExperiment::logcounts(pseudotime_sce)
+
+    if (split_by == "pseudotime_range") {
+      min_pdt = 0
+      max_pdt = ceiling(max(pseudotime))
+
+      bin_size = max_pdt/n_bins
+      bin_upper_limits = seq(bin_size, max_pdt, by=bin_size)
+
+      ## Put cells into the right bins
+      pseudotime_sce$pseudotime_bin = ceiling(pseudotime / bin_size)
+
+      pseudotime_sce$pseudotime_bin[pseudotime_sce$pseudotime_bin==0] = 1
+    } else {
+      pseudotime_order = order(pseudotime, decreasing = FALSE)
+
+      ncells = ncol(SingleCellExperiment::counts(pseudotime_sce))
+      cells_per_bin = floor(ncells/n_bins)
+      pseudotime_ordered_cells = rownames(pseudotime_sce@colData)[pseudotime_order]
+
+      pseudotime_sce$pseudotime_bin = n_bins
+
+      for (i in seq_len(n_bins)) {
+        cells_for_bin = pseudotime_ordered_cells[(i*cells_per_bin-cells_per_bin+1) : (i*cells_per_bin)]
+        pseudotime_sce[,cells_for_bin]$pseudotime_bin = i
+      }
+    }
+
+    bin_ids = unique(pseudotime_sce$pseudotime_bin)
+    pseudobulks = as.data.frame(Matrix::rowSums(SingleCellExperiment::logcounts(pseudotime_sce)))
+    for (i in bin_ids) {
+      pb = as.data.frame(Matrix::rowSums(SingleCellExperiment::logcounts(subset(pseudotime_sce, , pseudotime_sce@colData[["pseudotime_bin"]] == i))))
+      colnames(pb) = i
+      pseudobulks = cbind(pseudobulks, pb)
+    }
+    pseudobulks = pseudobulks[2:length(pseudobulks)]
+
+    return(methods::new("AtgnatData", pseudobulks = pseudobulks, bins = bin_ids))
+  }
+)
