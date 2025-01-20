b60ecd5da70aeacadbc644d47371d5b837dfa4c0
Andrew McCluskey <andrew@cluskii.com>
2025-01-20 16:03:50 +0000

    fixing r cmd check concerns

--- R/gene_selection.R
+++ R/gene_selection.R
@@ -274,7 +274,7 @@ PRIVATE_create_GAM <- function(to_smooth, knots) {
         mgcv::gam(
             nc ~ s(pdt, bs = "cr", k = knots),
             data = to_smooth,
-            family = gaussian(link = "log")
+            family = stats::gaussian(link = "log")
         )
     )
 }
