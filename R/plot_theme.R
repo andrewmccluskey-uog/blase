047faca80f1c7c937e086224c0af54ea440a28ad
Andrew McCluskey <andrew@cluskii.com>
2025-06-27 11:23:08 +0100

    Fix r cmd check

--- R/plot_theme.R
+++ R/plot_theme.R
@@ -1,16 +1,7 @@
 #' keywords internal
-blase_plots_theme <- function(rotate_x_labels = FALSE) {
-
-
-  if (rotate_x_labels) {
-    return(ggplot2::theme(
-      plot.title = element_text(face="bold", size = 12),
-      axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)))
-  } else {
-    return(ggplot2::theme(
-      plot.title = element_text(face="bold", size = 12)))
-  }
-
+blase_plots_theme <- function() {
+  return(ggplot2::theme(
+    plot.title = ggplot2::element_text(face="bold", size = 12)))
 }
 
 blase_titles_theme <- function() {
