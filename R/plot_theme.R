9ffb2198f772260a91294e4763a0d2cf407dd582
Andrew McCluskey <andrew@cluskii.com>
2025-07-03 11:23:25 +0100

    Make plot themes @internal

--- R/plot_theme.R
+++ R/plot_theme.R
@@ -1,9 +1,10 @@
-#' keywords internal
+#' @keywords internal
 blase_plots_theme <- function() {
   return(ggplot2::theme(
     plot.title = ggplot2::element_text(face="bold", size = 12)))
 }
 
+#' @keywords internal
 blase_titles_theme <- function() {
   return(ggplot2::theme(plot.title = ggplot2::element_text(size = 18)))
 }
