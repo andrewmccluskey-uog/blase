46477575b511c7ff566c0adf45122c39f7bd0e8b
Andrew McCluskey <andrew@cluskii.com>
2025-09-30 16:04:07 +0100

    more tests

--- tests/testthat/test-mapping_result_plotting.R
+++ tests/testthat/test-mapping_result_plotting.R
@@ -1,3 +1,16 @@
+# Tests for plot_mapping_result
+test_that("plot_mapping_result runs without error", {
+  sce <- generate_test_sce()
+  SingleCellExperiment::reducedDim(sce, "UMAP") <- uwot::umap(t(SingleCellExperiment::logcounts(sce)), n_neighbors = 2)
+  sce <- assign_pseudotime_bins(sce, n_bins=3, pseudotime_slot="pseudotime")
+  sce$cluster = round(runif(ncol(sce), 1, 3))
+  mapping_result <- generate_test_mapping_result()
+
+  tmp1 <- function() print(plot_mapping_result(sce, mapping_result, "cluster"))
+  expect_no_error(tmp1())
+})
+
+# Tests for plot_mapping_result_heatmap
 test_that("plot_mapping_result_heatmap throws error if list has no MappingResults (char)", {
     tmp1 <- function() plot_mapping_result_heatmap(list("abc"))
     expect_error(tmp1(), "You must provide a list of MappingResult objects only.", fixed = TRUE)
@@ -18,6 +31,31 @@ test_that("plot_mapping_result_heatmap throws error if list has a mix of Mapping
 test_that("plot_mapping_result_heatmap runs for list of MappingResults only", {
     mapping_result <- generate_test_mapping_result()
 
-    tmp1 <- function() plot_mapping_result_heatmap(list(mapping_result))
+    tmp1 <- function() print(plot_mapping_result_heatmap(list(mapping_result)))
     expect_no_error(tmp1())
+})
+
+test_that("plot_mapping_result_heatmap annotates best match boxes when annotate true", {
+  mapping_result <- generate_test_mapping_result()
+
+  p = plot_mapping_result_heatmap(list(mapping_result))
+  expect_s3_class(p, "ggplot")
+  expect_equal(p$layers$geom_tile$aes_params$linewidth, 0.8)
+  expect_equal(p$scales$scales[[2]]$breaks, c(FALSE, TRUE))
+
+  built_p = ggplot2::ggplot_build(p)
+  expect_equal(built_p$data[[1]]$colour, c("black", "transparent"))
+
+})
+
+test_that("plot_mapping_result_heatmap runs for list of MappingResults only", {
+  mapping_result <- generate_test_mapping_result()
+
+  p = plot_mapping_result_heatmap(list(mapping_result), annotate_confidence = FALSE)
+  expect_s3_class(p, "ggplot")
+  expect_equal(p$scales$scales[[2]]$breaks, c(FALSE, TRUE))
+
+  built_p = ggplot2::ggplot_build(p)
+  expect_equal(built_p$data[[1]]$colour, c("transparent", "transparent"))
+
 })
